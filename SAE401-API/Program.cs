23ababe0887c661d57582e198e4c1ad846c1bf7f
kaplego <70806521+kaplego@users.noreply.github.com>
2025-03-12 10:46:31 +0100

    .env CONNECTION_STRING

--- SAE401-API/Program.cs
+++ SAE401-API/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using SAE401_API.Models.EntityFramework;
+using DotNetEnv;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Pour ajouter la chaine de connexion, ajoutez un fichier .env avec comme contenu : CONNECTION_STRING="..."
+builder.Services.AddDbContext<_DBMilibooContext>(options =>
+  options.UseNpgsql(Environment.GetEnvironmentVariable("CONNECTION_STRING")));
 
 var app = builder.Build();
 
