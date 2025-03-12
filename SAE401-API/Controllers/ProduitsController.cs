239f0bd6e4367e48ca176df71bb9f58a6781ea25
lupin100 <clement.lacroix@etu.univ-smb.fr>
2025-03-12 16:14:28 +0100

    Produit Controllrt

---
+++ SAE401-API/Controllers/ProduitsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAE401_API.Models.EntityFramework;
+
+namespace SAE401_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProduitsController : ControllerBase
+    {
+        private readonly _DBMilibooContext _context;
+
+        public ProduitsController(_DBMilibooContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Produits
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produit>>> GetProduits()
+        {
+            return await _context.Produits.ToListAsync();
+        }
+
+        // GET: api/Produits/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produit>> GetProduit(int id)
+        {
+            var produit = await _context.Produits.FindAsync(id);
+
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            return produit;
+        }
+
+        // PUT: api/Produits/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduit(int id, Produit produit)
+        {
+            if (id != produit.Idproduit)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produit).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProduitExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Produits
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Produit>> PostProduit(Produit produit)
+        {
+            _context.Produits.Add(produit);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduit", new { id = produit.Idproduit }, produit);
+        }
+
+        // DELETE: api/Produits/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduit(int id)
+        {
+            var produit = await _context.Produits.FindAsync(id);
+            if (produit == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produits.Remove(produit);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProduitExists(int id)
+        {
+            return _context.Produits.Any(e => e.Idproduit == id);
+        }
+    }
+}
