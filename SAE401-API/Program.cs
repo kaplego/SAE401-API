f3d1f86779d6e186c483ddb693a0da1a0c7af2d8
ponchaue <ewan.ponchaud@etu.univ-smb.fr>
2025-03-12 17:06:17 +0100

    j'ai fait les trucs de JWT tokens mais il faudra modifier pour utiliser les users de la BDD

--- SAE401-API/Program.cs
+++ SAE401-API/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using SAE401_API.Models.EntityFramework;
 using DotNetEnv;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SAE401_API.Models;
+using System.Text;
 
 Env.Load();
 
@@ -17,6 +21,30 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<_DBMilibooContext>(options =>
   options.UseNpgsql(Environment.GetEnvironmentVariable("CONNECTION_STRING")));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"])),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization(config =>
+{
+    config.AddPolicy(Policies.Admin, Policies.AdminPolicy());
+    config.AddPolicy(Policies.User, Policies.UserPolicy());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +56,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
