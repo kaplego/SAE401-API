c22dd24cd9694d50c4f69e5c3c54dbad8bd2a890
millerob <millerob@etu.univ-smb.fr>
2025-03-26 15:25:29 +0100

    Utilisation de la clé API Program.cs inclus maintenant le middleware ApiKeyPolicy pour valider les clés api entrantes. Compare la clé dans le header "API_KEY" à la var d'env "API_KEY". Peut être désactivé avec .env "REQUIRE_API_KEY" (true/false) pour les tests et/ou l'implémentation

---
+++ SAE401-API/Models/ApiKeyPolicy.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SAE401_API.Models
+{
+    public class ApiKeyPolicy
+    {
+        private readonly RequestDelegate next;
+
+        public ApiKeyPolicy(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!bool.Parse(Environment.GetEnvironmentVariable("REQUIRE_API_KEY") ?? "true"))
+            {
+                await next(context);
+                return;
+            }
+
+            if (context.Request.Headers.TryGetValue("API_KEY", out var apiKey))
+            {
+                var key = apiKey.FirstOrDefault();
+                if (key == null)
+                {
+                    context.Response.StatusCode = 400;
+                }
+                else if (key != Environment.GetEnvironmentVariable("API_KEY"))
+                {
+                    context.Response.StatusCode = 403;
+                }
+                else
+                {
+                    await next(context);
+                    return;
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+            }
+        }
+    }
+}
--- SAE401-API/Program.cs
+++ SAE401-API/Program.cs
@@ -87,6 +87,8 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseMiddleware<ApiKeyPolicy>();
+
 app.MapControllers();
 
 app.Run();
